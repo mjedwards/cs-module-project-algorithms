b1b204a90c3a1e81c47796ff6d09f2d35064ddd2
Michael Edwards <ruoverlord@Michaels-MacBook-Pro.local>
2020-09-10 19:57:59 -0400

    initial commit

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,12 +3,24 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
+    jar = [0] * n
+    count = 0
+    # print(jar)
     # Your code here
-
-    pass
+    if len(jar) > 0:
+        count += 1
+        if n == 0: 
+            return 
+        return eating_cookies(n - 1)
+        
+    return count
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 2
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+
+# n = 3, 3 + (3 - 1)
+# n = 2, 2 + (2 - 1)
+# n = 1, 1
